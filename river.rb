405726ca37f18d1a2e983b9a6139e28dbd964698
Daniel Seehausen <daniel.seehausen@gmail.com>
2018-06-04 14:11:59 -0400

    added solution

--- river.rb
+++ river.rb
@@ -1,1 +1,21 @@
 # river.rb
+
+class River
+  
+  attr_accessor :name, :length, :countries, :discharge
+  
+  def initialize(name)
+    self.name = name
+    self.length = nil
+    self.countries = nil
+  end
+  
+  def flood
+    self.discharge *= 1.3
+  end
+  
+  def dry_up
+    self.discharge *= 0.5
+  end
+  
+end
